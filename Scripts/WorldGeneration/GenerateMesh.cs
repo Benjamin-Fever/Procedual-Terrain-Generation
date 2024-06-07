cfd1c320229bfc2cf984c730859d7d4e0e9d8bdf
Benjamin-Fever <ben.fever07@gmail.com>
2024-06-07 17:20:48 +1200

    first commit

---
+++ Scripts/WorldGeneration/GenerateMesh.cs
@@ -0,0 +1,118 @@
+﻿using Godot;
+
+namespace TerrainGeneration.Scripts.WorldGeneration;
+
+[GlobalClass, Tool]
+public partial class GenerateMesh : StaticBody3D {
+    private NoiseMap _noiseMap;
+
+    [Export]
+    private NoiseMap NoiseMap {
+        get => _noiseMap;
+        set {
+            _noiseMap = value;
+            if (Engine.IsEditorHint()) {
+                Clear();
+                _noiseMap.GenerateMap();
+                _noiseMap.MapGenerated += Generate;
+            }
+        }
+    }
+    [Export] private Curve _curve;
+    [Export] private float _heightMultiplier = 1.0f;
+    [Export] private int _lod = 1;
+
+    [Export] private Color grass;
+    [Export] private Color sand;
+    [Export] private Color water;
+    public void Generate() {
+        Clear();
+        SurfaceTool surfaceTool = new();
+        int subdivisions = _lod == 0 ? 1 : _lod * 2;
+        
+        int _chunkSize = _noiseMap.ChunkSize * 2 + 1;
+        float _halfChunkSize = _chunkSize / 2.0f;
+        
+        surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
+        Image image = Image.Create(_chunkSize, _chunkSize, false, Image.Format.Rgb8);
+        for (int y = 0; y < _chunkSize-subdivisions; y+=subdivisions) {
+            for (int x = 0; x < _chunkSize-subdivisions; x+=subdivisions) {
+                Vector3 vertex1 = new(
+                    x - _halfChunkSize, 
+                    _curve.Sample(_noiseMap.GetValue(x,y)) * _heightMultiplier, 
+                    y - _halfChunkSize);
+                Vector3 vertex2 = new(
+                    x + subdivisions - _halfChunkSize, 
+                    _curve.Sample(_noiseMap.GetValue(x+subdivisions,y)) * _heightMultiplier, 
+                    y - _halfChunkSize);
+                Vector3 vertex3 = new(
+                    x + subdivisions - _halfChunkSize,
+                    _curve.Sample(_noiseMap.GetValue(x + subdivisions, y + subdivisions)) * _heightMultiplier, 
+                    y + subdivisions - _halfChunkSize);
+                Vector3 vertex4 = new(
+                    x - _halfChunkSize,
+                    _curve.Sample(_noiseMap.GetValue(x, y + subdivisions)) * _heightMultiplier, 
+                    y + subdivisions - _halfChunkSize);
+
+                surfaceTool.SetUV(new Vector2(x/(float)_chunkSize, y/(float)_chunkSize));
+                surfaceTool.AddVertex(vertex1);
+                surfaceTool.SetUV(new Vector2((x + subdivisions) / (float)_chunkSize, y / (float)_chunkSize));
+                surfaceTool.AddVertex(vertex2);
+                surfaceTool.SetUV(new Vector2((x + subdivisions) / (float)_chunkSize, (y + subdivisions) / (float)_chunkSize));
+                surfaceTool.AddVertex(vertex3);
+                
+                surfaceTool.SetUV(new Vector2(x / (float)_chunkSize, y / (float)_chunkSize));
+                surfaceTool.AddVertex(vertex1);
+                surfaceTool.SetUV(new Vector2((x + subdivisions) / (float)_chunkSize, (y + subdivisions) / (float)_chunkSize));
+                surfaceTool.AddVertex(vertex3);
+                surfaceTool.SetUV(new Vector2(x / (float)_chunkSize, (y + subdivisions) / (float)_chunkSize));
+                surfaceTool.AddVertex(vertex4);
+                
+                float height = _noiseMap.GetValue(x,y);
+                image.SetPixel(x,y,Colors.Black.Lerp(Colors.White, height));
+                
+                // if (height > 0.65f) {
+                //     image.SetPixel(x,y,grass);
+                // }
+                // else if (height > 0.58f) {
+                //     image.SetPixel(x, y, sand);
+                // }
+                // else {
+                //     image.SetPixel(x, y, water);
+                // }
+            }
+        }
+        surfaceTool.GenerateNormals();
+        ImageTexture texture = new();
+        texture.SetImage(image);
+        MeshInstance3D meshInstance = new() {
+            Mesh = surfaceTool.Commit(),
+            MaterialOverlay = new StandardMaterial3D() {
+                AlbedoTexture = texture,
+                TextureFilter = BaseMaterial3D.TextureFilterEnum.Nearest
+            }
+        };
+        AddChild(meshInstance);
+        meshInstance.Owner = Owner;
+    }
+
+    public void Clear() {
+        foreach (Node child in GetChildren()) {
+            child.QueueFree();
+        }
+    }
+    
+    #if TOOLS
+    public void ExtendInspectorBegin(ExtendableInspector inspector) {
+        Button button = new() {
+            Text = "Generate"
+        };
+        button.Pressed += () => {
+            Clear();
+            Generate();
+        };
+        inspector.AddCustomControl(button);
+    }
+    #endif
+}
+
---
+++ Scripts/WorldGeneration/NoiseMap.cs
@@ -0,0 +1,135 @@
+﻿using Godot;
+
+namespace TerrainGeneration.Scripts.WorldGeneration;
+
+[GlobalClass, Tool]
+public partial class NoiseMap : Resource  {
+    [Signal] public delegate void MapGeneratedEventHandler();
+    private int _chunkSize = 200;
+    private float _noiseScale = 1.0f;
+    private int octaves = 5;
+    private float persistence = 0.5f;
+    private float lacunarity = 2.0f;
+    private int _seed = 0;
+    private Vector2 _offset = Vector2.Zero;
+    private FastNoiseLite _noise = new FastNoiseLite() { FractalType = FastNoiseLite.FractalTypeEnum.None };
+    private float[,] _noiseMap;
+    
+    [Export] public int ChunkSize {
+        get => _chunkSize;
+        set {
+            _chunkSize = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    [Export] public int Seed {
+        get => _seed;
+        set {
+            _seed = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    [Export] public FastNoiseLite.NoiseTypeEnum NoiseType {
+        get => _noise.NoiseType;
+        set {
+            _noise.NoiseType = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    [Export] public float NoiseScale {
+        get => _noiseScale;
+        set {
+            _noiseScale = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    [Export] public int Octaves {
+        get => octaves;
+        set {
+            octaves = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    [Export] public float Persistence {
+        get => persistence;
+        set {
+            persistence = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    [Export] public float Lacunarity {
+        get => lacunarity;
+        set {
+            lacunarity = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    [Export] public Vector2 Offset {
+        get => _offset;
+        set {
+            _offset = value;
+            if (Engine.IsEditorHint()) GenerateMap();
+        }
+    }
+    
+    public NoiseMap() {
+        GenerateMap();
+    }
+    public void GenerateMap() {
+        int size = _chunkSize * 2 + 1;
+        float[,] noiseMap = new float[size, size];
+        float maxNoise = float.MinValue;
+        float minNoise = float.MaxValue;
+        float amplitude = 1;
+        float frequency = 1;
+        float maxPossibleHeight = 0;
+
+        float _halfChunkSize = size / 2f;
+    
+        System.Random prng = new System.Random(_seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++) {
+            float offsetX = prng.Next(-100000, 100000) + _offset.X;
+            float offsetY = prng.Next(-100000, 100000) + _offset.Y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+            
+            maxPossibleHeight += amplitude;
+            amplitude *= persistence;
+        }
+    
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                float noiseHeight = 0;
+                amplitude = 1;
+                frequency = 1;
+                for (int i = 0; i < octaves; i++) {
+                    float sampleX = (x - _halfChunkSize) / _noiseScale * frequency + octaveOffsets[i].X * frequency;
+                    float sampleY = (y - _halfChunkSize) / _noiseScale * frequency + octaveOffsets[i].Y * frequency;
+
+                    float perlinValue = _noise.GetNoise2D(sampleX, sampleY);
+                    noiseHeight += perlinValue * amplitude;
+                
+                    amplitude *= persistence;
+                    frequency *= lacunarity;
+                
+                }
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+        
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                float normalizedHeight = (noiseMap[x, y] + 1) / (2f * maxPossibleHeight / 1.75f);
+                noiseMap[x, y] = normalizedHeight;
+            }
+        }
+    
+        _noiseMap = noiseMap;
+        EmitSignal(SignalName.MapGenerated);
+    }
+
+    public float GetValue(int x, int y) {
+        return _noiseMap[x, y];
+    }
+}
